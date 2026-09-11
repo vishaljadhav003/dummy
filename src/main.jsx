b916e5812e71e10646dba15036d530d0e808a270
Vishal Jadhav <vj042459@gmail.com>
2026-09-11 12:25:14 +0530

    Fix SEO dependencies and HelmetProvider

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import App from "./App";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
   </React.StrictMode>
 );
